cde5274aed66c205d55504fd8d6f405131aacb8c
nicefeiniu <872938058@qq.com>
2023-04-28 22:59:09 +0800

    feat: 先选择大类，再选择热门的标签

--- News-Page/src/views/Register.vue
+++ News-Page/src/views/Register.vue
@@ -9,7 +9,7 @@
       <div class="input-c">
         <Input  v-model="username" prefix="md-lock" placeholder="用户名" clearable @on-blur="verifyUsername"
                @keyup.enter.native="submit" />
-        <p class="error">{{pwdError}}</p>
+        <p class="error">{{usernameError}}</p>
       </div>
       <div class="input-c">
         <Input type="password" v-model="pwd" prefix="md-lock" placeholder="密码" clearable @on-blur="verifyPwd"
@@ -47,7 +47,12 @@
       </div>
       <div class="wordcloud" id="wordcloud" style="width:500px;height:500px;background: #ffffff; "></div>
     </Modal>
-
+    <Modal v-model="bigTypeModel" title="请选择新闻类别"  @on-ok="submitBigType">
+      <div class="tagspace">
+        <Tag v-for="(item,index) in taglist2" :key="index" :name="item" closable size="large" @on-close="deleteTags(item)">{{ item }}</Tag>
+      </div>
+      <div class="wordcloud" id="wordcloud2" style="width:500px;height:500px;background: #ffffff; "></div>
+    </Modal>
   </div>
 </template>
 
@@ -62,6 +67,9 @@ export default {
   },
   data() {
     return {
+      bigTypes: ['美股', '股市', '财经', '科技', '军事', '娱乐', '体育', '社会', '国际', '国内'],
+      bigTypeModel: false,
+      usernameError: '',
       studentCode: '',
       phone: '',
       email: '',
@@ -81,8 +89,11 @@ export default {
       wordlist: [],
       word: false,
       charts: '',
+      charts2: '',
       cloud: [],
+      cloud2: [],
       taglist: [],
+      taglist2: [],
       account: '',
       pwd: '',
       accountError: '',
@@ -236,9 +247,95 @@ export default {
         this.charts.setOption(option,true)
       }
     },
+    eConsole2(param) {
+      console.log(param)
+      if (typeof param.seriesIndex != 'undefined') {
+        console.log(param.name)
+        this.taglist2.push(param.name)
+        // console.log(this.charts)
+        for (let i in this.cloud2){
+          // console.log(i)
+          if (this.cloud2[i].name === param.name){
+            this.cloud2.splice(i,1)
+            // console.log(this.cloud[i])
+          }
+        }
+        console.log(this.cloud2)
+        let option = {
+          series: [{
+            type: 'wordCloud',
+            shape: 'circle',
+            left: 'center',
+            top: 'center',
+            width: '70%',
+            height: '80%',
+            right: null,
+            bottom: null,
+            // clickable : true,
+            // Text size range which the value in data will be mapped to.
+            // Default to have minimum 12px and maximum 60px size.
+
+            sizeRange: [12, 60],
+
+            // Text rotation range and step in degree. Text will be rotated randomly in range [-90, 90] by rotationStep 45
+
+            rotationRange: [0, 0],
+            rotationStep: 90,
+
+            // size of the grid in pixels for marking the availability of the canvas
+            // the larger the grid size, the bigger the gap between words.
+
+            gridSize: 8,
+
+            // set to true to allow word being draw partly outside of the canvas.
+            // Allow word bigger than the size of the canvas to be drawn
+            drawOutOfBound: false,
+
+            // If perform layout animation.
+            // NOTE disable it will lead to UI blocking when there is lots of words.
+            layoutAnimation: true,
+
+            // Global text style
+            textStyle: {
+              fontFamily: 'sans-serif',
+              fontWeight: 'bold',
+              // Color can be a callback function or a color string
+              color: function () {
+                // Random color
+                return 'rgb(' + [
+                  Math.round(Math.random() * 160),
+                  Math.round(Math.random() * 160),
+                  Math.round(Math.random() * 160)
+                ].join(',') + ')';
+              }
+            },
+            emphasis: {
+              focus: 'self',
+
+              textStyle: {
+                shadowBlur: 10,
+                shadowColor: '#333'
+              }
+            },
+
+            // Data is an array. Each array item must have name and value property.
+            data: this.cloud2,
+
+          }]
+        }
+        this.charts2.setOption(option,true)
+
+        this.submitBigType()
+      }
+    },
     verifyUserid() {
       if (this.account === '') {
         this.accountError = '账号不能为空'
+      }
+    },
+    verifyUsername(){
+      if (this.username === '' ) {
+        this.usernameError = '用户名不能为空'
       }
     },
     verifyStudentCode() {
@@ -271,8 +368,92 @@ export default {
       this.$router.push({ name: 'login' })
     },
     submit() {
-      this.word=true
+      if (this.taglist2.length > 0) {
+        this.submitBigType()
+        // this.initCharts2()
+      } else {
+        this.bigTypeModel=true
+
+      }
     },
+    submitBigType(){
+      this.bigTypeModel = false
+      this.word = true
+    },
+    initCharts2(){
+      for (let i in this.bigTypes){
+        let data = {
+          name:this.bigTypes[i],
+          value: Math.random()*50
+        }
+        this.cloud2.push(data)
+      }
+      let chart = echarts.init(document.getElementById('wordcloud2'))
+      chart.on('click', this.eConsole2);
+      chart.setOption({
+        series: [{
+          type: 'wordCloud',
+          shape: 'circle',
+          left: 'center',
+          top: 'center',
+          width: '70%',
+          height: '80%',
+          right: null,
+          bottom: null,
+          // clickable : true,
+          // Text size range which the value in data will be mapped to.
+          // Default to have minimum 12px and maximum 60px size.
+
+          sizeRange: [12, 60],
+
+          // Text rotation range and step in degree. Text will be rotated randomly in range [-90, 90] by rotationStep 45
+
+          rotationRange: [0, 0],
+          rotationStep: 90,
+
+          // size of the grid in pixels for marking the availability of the canvas
+          // the larger the grid size, the bigger the gap between words.
+
+          gridSize: 8,
+
+          // set to true to allow word being draw partly outside of the canvas.
+          // Allow word bigger than the size of the canvas to be drawn
+          drawOutOfBound: false,
+
+          // If perform layout animation.
+          // NOTE disable it will lead to UI blocking when there is lots of words.
+          layoutAnimation: true,
+
+          // Global text style
+          textStyle: {
+            fontFamily: 'sans-serif',
+            fontWeight: 'bold',
+            // Color can be a callback function or a color string
+            color: function () {
+              // Random color
+              return 'rgb(' + [
+                Math.round(Math.random() * 160),
+                Math.round(Math.random() * 160),
+                Math.round(Math.random() * 160)
+              ].join(',') + ')';
+            }
+          },
+          emphasis: {
+            focus: 'self',
+
+            textStyle: {
+              shadowBlur: 10,
+              shadowColor: '#333'
+            }
+          },
+
+          // Data is an array. Each array item must have name and value property.
+          data: this.cloud2,
+
+        }]
+      })
+      this.charts2 = chart
+    }
   },
   mounted() {
     getTags().then(res => {
@@ -350,6 +531,7 @@ export default {
       })
       this.charts = chart
     })
+    this.initCharts2();
   }
 }
 </script>
